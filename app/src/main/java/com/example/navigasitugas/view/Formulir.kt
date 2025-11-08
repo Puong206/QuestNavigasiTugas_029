a2ab2d98fb5525d6e685bf42c52e55c8e267418b
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-11-09 01:25:27 +0700

    Menambahkan dependensi `material-icons-extended`

--- app/src/main/java/com/example/navigasitugas/view/Formulir.kt
+++ app/src/main/java/com/example/navigasitugas/view/Formulir.kt
@@ -14,10 +14,9 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.*
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
-import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
