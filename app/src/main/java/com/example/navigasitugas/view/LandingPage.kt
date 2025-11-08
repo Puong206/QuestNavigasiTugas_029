8c9334d29f01d2eaf58c84bb1ad86bdad8db840a
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-11-08 13:23:10 +0700

    Menambahkan font Plus Jakarta Sans pada LandingPage.kt

--- app/src/main/java/com/example/navigasitugas/view/LandingPage.kt
+++ app/src/main/java/com/example/navigasitugas/view/LandingPage.kt
@@ -2,7 +2,22 @@ package com.example.navigasitugas.view
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.ExperimentalMaterial3Api
+import com.example.navigasitugas.R
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+
+val PlusJakartaSans = FontFamily(
+    Font(R.font.plusjakartasans_extralight, FontWeight.ExtraLight),
+    Font(R.font.plusjakartasans_light, FontWeight.Light),
+    Font(R.font.plusjakartasans_regular, FontWeight.Normal),
+    Font(R.font.plusjakartasans_medium, FontWeight.Medium),
+    Font(R.font.plusjakartasans_semibold, FontWeight.SemiBold),
+    Font(R.font.plusjakartasans_bold, FontWeight.Bold),
+    Font(R.font.plusjakartasans_extrabold, FontWeight.ExtraBold)
+)
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
