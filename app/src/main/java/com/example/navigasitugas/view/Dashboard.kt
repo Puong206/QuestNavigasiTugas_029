2eb41189e692292a830da9c938ec07dd30e90e2f
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-11-08 15:15:58 +0700

    Membuat file Dashboard.kt

--- app/src/main/java/com/example/navigasitugas/view/Dashboard.kt
+++ app/src/main/java/com/example/navigasitugas/view/Dashboard.kt
@@ -1,2 +1,36 @@
 package com.example.navigasitugas.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import com.example.navigasitugas.R
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Dashboard() {
+    Scaffold(modifier = Modifier,
+        topBar = {
+            //Image(pointerResource(id = R.drawable.listdata))
+            TopAppBar(
+                title = { Text("Dashboard",
+                    fontFamily = PlusJakartaSans,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 20.sp)
+                }
+            )
+        }) {
+        Frame->
+        Column(modifier = Modifier.padding(Frame)) {
+
+        }
+    }
+}
