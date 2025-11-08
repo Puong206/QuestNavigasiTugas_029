82c5f63d572deee6252023a64509d2ad4c302b29
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-11-08 14:53:57 +0700

    Menyesuaikan UI pada LandingPage.kt

--- app/src/main/java/com/example/navigasitugas/view/LandingPage.kt
+++ app/src/main/java/com/example/navigasitugas/view/LandingPage.kt
@@ -1,6 +1,8 @@
 package com.example.navigasitugas.view
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.hoverable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -17,6 +19,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import com.example.navigasitugas.R
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -47,7 +50,7 @@ fun LandingPage(
         color = colorResource(id = R.color.blue)) {
         Column(modifier = Modifier
             .fillMaxSize()
-            .padding(top = 40.dp, bottom = 32.dp),
+            .padding(top = 20.dp, bottom = 20.dp),
             verticalArrangement = Arrangement.SpaceAround,
             horizontalAlignment = Alignment.CenterHorizontally) {
             Text("Selamat Datang",
@@ -56,7 +59,6 @@ fun LandingPage(
                 fontSize = 32.sp,
                 fontWeight = FontWeight.Bold
             )
-            //Spacer(modifier = Modifier.height(80.dp))
             val logo = painterResource(id = R.drawable.listdata)
             Image(painter = logo,
                 contentScale = ContentScale.Fit,
@@ -64,7 +66,6 @@ fun LandingPage(
                 modifier = Modifier
                     .height(48.dp)
             )
-            //Spacer(modifier = Modifier.height(80.dp))
             Column(modifier = Modifier
                 .fillMaxWidth(),
                 verticalArrangement = Arrangement.SpaceAround,
@@ -74,7 +75,7 @@ fun LandingPage(
                     color = colorResource(id = R.color.white),
                     fontFamily = PlusJakartaSans,
                     fontSize = 16.sp,
-                    fontWeight = FontWeight.Normal
+                    fontWeight = FontWeight.Medium
                 )
                 Text("20230140029",
                     color = colorResource(id = R.color.white),
@@ -83,12 +84,15 @@ fun LandingPage(
                     fontWeight = FontWeight.Light
                 )
                 Spacer(modifier = Modifier.height(16.dp))
+                val hover = remember { MutableInteractionSource() }
                 ElevatedButton(
                     colors = ButtonDefaults.buttonColors(
                         containerColor = colorResource(id = R.color.orange),
                         contentColor = colorResource(id = R.color.white)
                     ),
-                    modifier = Modifier.width(240.dp),
+                    modifier = Modifier
+                        .width(240.dp),
+                        //.hoverable(interactionSource = hover),
                     onClick = onBackButtonClick
                 ) {
                     Text(text = "Masuk",
