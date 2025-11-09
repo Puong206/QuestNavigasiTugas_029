1db949e8303988ee6afb19d847425ffcdf605faa
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-11-10 03:44:04 +0700

    Membuat `enum class` untuk Navigasi

--- app/src/main/java/com/example/navigasitugas/Navigasi.kt
+++ app/src/main/java/com/example/navigasitugas/Navigasi.kt
@@ -4,7 +4,11 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 
-
+enum class Navigasi {
+    LandingPage,
+    Dashboard,
+    Formulir
+}
 
 @Composable
 fun App(
