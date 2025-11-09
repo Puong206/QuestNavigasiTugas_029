e4f0607598f21fee4c3444a9806d25041d9adaff
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-11-10 04:07:22 +0700

    Menerapkan `NavHost` untuk navigasi antar halaman

--- app/src/main/java/com/example/navigasitugas/Navigasi.kt
+++ app/src/main/java/com/example/navigasitugas/Navigasi.kt
@@ -1,18 +1,51 @@
 package com.example.navigasitugas
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.navigasitugas.view.Dashboard
+import com.example.navigasitugas.view.LandingPage
 
 enum class Navigasi {
-    LandingPage,
-    Dashboard,
-    Formulir
+    Start,
+    Home,
+    Add
 }
 
 @Composable
 fun App(
     navController: NavHostController = rememberNavController()
 ){
+    Scaffold { frame1 ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Start.name,
+            modifier = Modifier.padding(frame1))
+        {
+            composable(route = Navigasi.Start.name) {
+                LandingPage(
+                    onStartButtonClick = { navController.navigate(Navigasi.Home.name)}
+                )
+            }
+            composable(route = Navigasi.Home.name) {
+                Dashboard(
+                    onExitButtonClick = {cancelAndBackToStart(navController)},
+                    onFormButtonClick = {}
+                )
+            }
+        }
+    }
+}
 
+private fun cancelAndBackToStart(
+    navController: NavHostController
+) {
+    navController.popBackStack(Navigasi.Start.name, inclusive = false)
 }
