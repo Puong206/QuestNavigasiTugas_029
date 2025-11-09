82c8a91db9df267122c226eb207d746cdd2acb97
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-11-10 03:43:42 +0700

    Membuat App composable di Navigasi.kt

--- app/src/main/java/com/example/navigasitugas/Navigasi.kt
+++ app/src/main/java/com/example/navigasitugas/Navigasi.kt
@@ -1,2 +1,14 @@
 package com.example.navigasitugas
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+
+
+@Composable
+fun App(
+    navController: NavHostController = rememberNavController()
+){
+
+}
