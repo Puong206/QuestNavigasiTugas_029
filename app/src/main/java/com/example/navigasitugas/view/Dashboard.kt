14971b085c70d44e5ca4be4ad3d649de13b1a28f
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-11-10 08:41:42 +0700

    Menambahkan `shimmer-effect` pada `Dashboard`

--- app/src/main/java/com/example/navigasitugas/view/Dashboard.kt
+++ app/src/main/java/com/example/navigasitugas/view/Dashboard.kt
@@ -1,5 +1,11 @@
 package com.example.navigasitugas.view
 
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -37,10 +43,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.TileMode
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -96,6 +107,31 @@ fun GlassCard(
     }
 }
 
+fun Modifier.shimmerBackground(shape: Shape = RectangleShape): Modifier = composed {
+    val transition = rememberInfiniteTransition()
+
+    val translateAnimation by transition.animateFloat(
+        initialValue = 0f,
+        targetValue = 400f,
+        animationSpec = infiniteRepeatable(
+            tween(durationMillis = 1500, easing = LinearEasing),
+            RepeatMode.Restart
+        ),
+    )
+    val shimmerColors = listOf(
+        Color.LightGray.copy(alpha = 0.9f),
+        Color.LightGray.copy(alpha = 0.4f),
+    )
+    val brush = Brush.linearGradient(
+        colors = shimmerColors,
+        start = Offset(translateAnimation, translateAnimation),
+        end = Offset(translateAnimation + 100f, translateAnimation + 100f),
+        tileMode = TileMode.Mirror,
+    )
+    return@composed this.then(
+        Modifier.background(brush, shape)
+    )
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
